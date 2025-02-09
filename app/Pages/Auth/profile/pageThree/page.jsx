c0b9a9af7276da311904772e67616402cd2125d2
keertimaanya <keertimaanya06138@gmail.com>
2025-02-09 14:23:56 +0530

    Navigation buttons modified

--- app/Pages/Auth/profile/pageThree/page.jsx
+++ app/Pages/Auth/profile/pageThree/page.jsx
@@ -113,7 +113,7 @@ const WorkSelection = () => {
           </button>
 
           <button 
-            className="bg-green-600 text-white py-2 px-4 rounded-lg hover:bg-green-700"
+            className="bg-blue-900 text-white py-2 px-4 rounded-lg hover:bg-blue-700"
             onClick={() => router.push("pageFive")}
           >
             Next, add your skills
